95b6a17e1836f0276db7c762fe77d9bdf97d666f
abhishek devop <standup787059@gmail.com>
2025-09-03 01:01:49 +0530

    user authentication

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const authContoller = require('../controllers/auth.controller')
 
 const router = express.Router()
 
-router.post('/user/register', )
+router.post('/user/register', authContoller.registerUser)
+router.post('/user/login', authContoller.loginUser)
+router.get('/user/logout', authContoller.logoutUser)
 
 
 module.exports = router
